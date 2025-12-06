dfe65513b54bbc0e979b27a1cabb64cde6543643
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-12-06 20:02:50 +0200

    Update app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const resultElement = document.getElementById('result');
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+const submitBtn = document.getElementById('submit');
+
+const sum = Number(input1.value) + Number(input2.value);
+resultElement.textContent = sum;
